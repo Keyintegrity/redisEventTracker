46038603314ac10b5614c2949608343e1a65d279
uishnik <uishnik@yandex.ru>
2015-05-28 11:27:42 +0300

    version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setup(
     name='redis-event-tracker',
-    version='0.0.9',
+    version='0.1.0',
     packages=find_packages(),
     requires=['redis (>= 2.7.6)'],
     description='Track your events in redis',
